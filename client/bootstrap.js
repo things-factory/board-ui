2207810c948578d513d72b7be65be2226383335f
Hearty Oh <heartyoh@hatiolab.com>
2019-07-05 23:08:13 +0900

    v0.2.2

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,7 +1,6 @@
 import { html } from 'lit-html'
 
 import { store } from '@things-factory/shell'
-import { i18next } from '@things-factory/i18n-base'
 import { addRoutingType } from '@things-factory/menu-base'
 
 import board from './reducers/board'
@@ -15,10 +14,6 @@ import {
 } from '@things-factory/layout-base'
 
 export default function bootstrap() {
-  i18next.loadNamespaces('board-ui', err => {
-    err && console.error(err)
-  })
-
   store.dispatch(addRoutingType('VIEWER', 'board-viewer'))
   store.dispatch(addRoutingType('PLAYER', 'board-player'))
 
