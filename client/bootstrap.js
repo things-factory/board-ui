3cfcf03a743b2c27c67c13d9996684c50f4087e5
lllleeee <lcj1123@hatiolab.com>
2019-07-26 21:04:03 +0900

    work in assign font

--- client/bootstrap.js
+++ client/bootstrap.js
@@ -1,7 +1,20 @@
 import { store } from '@things-factory/shell'
 
 import board from './reducers/board'
+import { fetchFontList } from '@things-factory/font-base'
 
 export default function bootstrap() {
   store.addReducers({ board })
+
+  var lastUser
+  store.subscribe(() => {
+    var state = store.getState()
+    var user = state.auth.user
+
+    if(user && user !== lastUser) {
+      lastUser = user
+
+      store.dispatch(fetchFontList())
+    }
+  })
 }
