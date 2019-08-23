31df74f8f94233e9ddb2a8bff5c31f42f3f4e734
lllleeee <lcj1123@hatiolab.com>
2019-08-23 20:38:49 +0900

    Update modeller-module.js

--- client/modeller-module.js
+++ client/modeller-module.js
@@ -1,1 +1,2 @@
 export { default as ThingsEditorProperty } from './board-modeller/editors/things-editor-property'
+export { ThingsEditorPropertyStyles } from './board-modeller/editors/things-editor-property-styles'
