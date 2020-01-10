c5ad2f2bdf9340263f199a697c12793b9c68c252
Hearty Oh <heartyoh@hatiolab.com>
2020-01-10 15:50:03 +0900

    플레이모드에서는 각 뷰어마다 board-provider를 제공하도록 함

--- client/board-provider.js
+++ client/board-provider.js
@@ -2,39 +2,45 @@ import gql from 'graphql-tag'
 import { client } from '@things-factory/shell'
 import { ReferenceMap, create, error } from '@hatiolab/things-scene'
 
-export const provider = new ReferenceMap(
-  async (boardId, resolve, reject) => {
-    try {
-      const response = await client.query({
-        query: gql`
-          query FetchBoardById($id: String!) {
-            board(id: $id) {
-              model
+export function createBoardProvider() {
+  var _provider = new ReferenceMap(
+    async (boardId, resolve, reject) => {
+      try {
+        const response = await client.query({
+          query: gql`
+            query FetchBoardById($id: String!) {
+              board(id: $id) {
+                model
+              }
             }
-          }
-        `,
-        variables: { id: boardId }
-      })
+          `,
+          variables: { id: boardId }
+        })
 
-      const board = response.data.board
-      var model = JSON.parse(board.model)
+        const board = response.data.board
+        var model = JSON.parse(board.model)
 
-      var scene = create({
-        model,
-        mode: 0,
-        refProvider: provider
-      })
+        var scene = create({
+          model,
+          mode: 0,
+          refProvider: _provider
+        })
 
-      resolve(scene, {
-        ...board,
-        model
-      })
-    } catch (e) {
-      error(e)
-      reject(e)
+        resolve(scene, {
+          ...board,
+          model
+        })
+      } catch (e) {
+        error(e)
+        reject(e)
+      }
+    },
+    async (id, ref) => {
+      ref.dispose()
     }
-  },
-  async (id, ref) => {
-    ref.dispose()
-  }
-)
+  )
+
+  return _provider
+}
+
+export const provider = createBoardProvider()
